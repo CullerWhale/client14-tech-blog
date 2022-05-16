66068e76b1764a30e9b0a6a4654313c5fe1deaf3
CullerWhale <jcullenharris@gmail.com>
2022-05-15 19:24:17 -0500

    adding handlebars to week13

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const Category = require('../models/Category.js');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+// router.use((req, res) => {
+//   res.send("<h1>Wrong Route!</h1>")
+// });
+
+//handlebar homepage
+router.get('/', (req, res) => {
+  res.render('homepage')
+} )
+
+//get all categories
+router.get('/category', async (req, res) => {
+  const data = await Category.findAll()
+  const neededData = data.map((pizza) =>
+    pizza.get({plain: true}))
+    console.log(neededData);
+
+  res.render('oneCategory', {neededData, loggedIn: true});
+  // res.render('oneCategory', {neededData, req.session.loggedIn });       module 14 sessions and class activities
+  //api route for user activity number 4
+  //middleware one line on server next activity number 5
 });
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,23 @@ const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
 
+//handle
+const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
+
+//middleware
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//handle
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
